d6ece9b7fcccd37d30fab7d6ee3233a4e5f730a6
Sarra <sarra.tounsi@esprit.tn>
2024-02-02 11:52:06 +0100

    choix css

--- server/bnclean.js
+++ server/bnclean.js
@@ -1,19 +1,28 @@
 const express = require('express');
 const participation = require('./src/routes/participation'); 
+const session = require('express-session');
+
 const app = express();
 const PORT = 8081;
 const cors = require('cors');
 const path = require('path');
 
 app.use(express.static('public'))
-
+app.use(session({
+  secret: 'your-secret-key', 
+  resave: true,
+  saveUninitialized: true
+}));
 app.use(express.json());
 app.use(cors());
 app.use('/api', participation); 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/pages/index.html'));
+    req.session.participationId = ;
   });
   app.get('/choix', (req, res) => {
+    const participationId = req.session.participationId;
+
     res.sendFile(path.join(__dirname, '../client/pages/choix.html'));
   });
 
